46772382d222873919f7e9b0bed8be17ed0a5ae2
John Fulton <fulton@redhat.com>
2019-11-22 02:44:50 +0000

    support mk_ext_ceph.sh -name

--- glance/mk_ext_ceph.sh
+++ glance/mk_ext_ceph.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-INV=config-download/ceph-ansible/inventory.yml
+CEPH=$1
+if [[ -z $CEPH ]]; then CEPH=dcn0; fi
+echo "Extracting ceph client information for ceph cluster: $CEPH"
+
+INV=$CEPH/config-download/ceph-ansible/inventory.yml
 if [[ ! -e $INV ]]; then
     echo "Missing config download inventory: $INV"
     exit 1
 fi
 
-ANS="ansible -i $INV CephAll[0] -m shell -b"
-FSID=$($ANS -a "cat /etc/ceph/ceph.conf" | grep "fsid" | awk 'BEGIN { FS = "= " } ; { print $2 }')
-KEY=$($ANS -a "cat /etc/ceph/ceph.client.openstack.keyring" | grep key | awk 'BEGIN { FS = "= " } ; { print $2 }')
-IPS=$($ANS -a "cat /etc/ceph/ceph.conf" | grep "mon host" | awk 'BEGIN { FS = "= " } ; { print $2 }')
+ANS="ansible -i $INV DistributedComputeHCI[0] -m shell -b"
+FSID=$($ANS -a "cat /etc/ceph/$CEPH.conf" | grep "fsid" | awk 'BEGIN { FS = "= " } ; { print $2 }')
+KEY=$($ANS -a "cat /etc/ceph/$CEPH.client.openstack.keyring" | grep key | awk 'BEGIN { FS = "= " } ; { print $2 }')
+IPS=$($ANS -a "cat /etc/ceph/$CEPH.conf" | grep "mon host" | awk 'BEGIN { FS = "= " } ; { print $2 }')
 
 cat <<EOF > external_ceph.yaml
 parameter_defaults:
+  CephClusterName: $CEPH
   CephClusterFSID: "$FSID"
   CephClientKey: "$KEY"
   CephExternalMonHost: "$IPS"
 EOF
-wc -l external_ceph.yaml
+echo ""
 cat external_ceph.yaml
 
