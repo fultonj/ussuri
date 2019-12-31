6b3c264d9bef7ed8b2e30d6c484234d64402cceb
John Fulton <fulton@redhat.com>
2019-12-31 21:58:42 +0000

    log ansible run

--- glance/ansible/run.sh
+++ glance/ansible/run.sh
@@ -3,6 +3,7 @@
 source ~/stackrc
 STACKS="control-plane,dcn0,dcn1"
 export ANSIBLE_DEPRECATION_WARNINGS=0
+export ANSIBLE_LOG_PATH="ansible.log"
 INV=inventory.yml
 if [[ ! -e $INV ]]; then
     tripleo-ansible-inventory --static-yaml-inventory $INV --stack $STACKS
