616a5a69082673a447d67d8c6a042566a7e1d99e
John Fulton <fulton@redhat.com>
2020-01-12 18:02:44 +0000

    add multiceph/validate scripts

--- multiceph/test_ceph_client.sh
+++ multiceph/test_ceph_client.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-NAME=ceph
+if [ -z "$1" ]; then
+    NAME=ceph
+else
+    NAME=$1
+fi
+
+echo "Testing if $HOSTNAME can be a client to the ceph cluster called $NAME"
+
 SVC=openstack
 POOL=images
 CONF=/etc/ceph/$NAME.conf
-ID=client.$SVC
+ID=$SVC
 KEY=/etc/ceph/$NAME.client.$SVC.keyring
-RBD="rbd --conf $CONF --id $ID --keyring $KEY --cluster cent0"
+RBD="rbd --conf $CONF --keyring $KEY --id $ID --cluster $NAME"
 DATA=$(date | md5sum | cut -c-12)
 
 if [[ ! -e /usr/bin/rbd ]]; then
@@ -16,21 +23,24 @@ if [[ ! -e /usr/bin/crudini ]]; then
     sudo yum install -y crudini
 fi
 
-MON=$(crudini --get $CONF global 'mon host' | awk 'BEGIN { FS = ":" } ; { print $2 }')
+MON=$(crudini --get $CONF global 'mon host')
 ping -q -c 1 $MON > /dev/null 2>&1
 if [[ $? -gt 0 ]]; then
     echo "Unable to ping host: $MON exiting"
     exit 1
 fi
 
-# read
+echo -e "\nCeph commands will be like:\n"
+echo "sudo $RBD ls $POOL"
+
+echo -e "\nRead (pool might be empty)\n"
 sudo $RBD ls $POOL
-exit 0
-# write
+
+echo -e "\nWrite $DATA to $POOL\n"
 sudo $RBD create --size 1024 $POOL/$DATA
 
-# read
+echo -e "\nRead list data in $POOL\n"
 sudo $RBD ls $POOL
 
-# delete
+echo -e "\nDelete $DATA from $POOL\n"
 sudo $RBD rm $POOL/$DATA
---
+++ multiceph/validate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Can $CONTROLLER use ceph cluster(s) named in $CLUSTERS?
+
+CLUSTERS="ceph0"
+#CLUSTERS="ceph0 ceph1"
+FILE=test_ceph_client.sh
+source ~/stackrc
+CONTROLLER=$(openstack server list -c Networks -c Name -f value | grep controller-0 | awk {'print $2'} | sed s/ctlplane=//g)
+
+scp -q -o "StrictHostKeyChecking no" $FILE heat-admin@$CONTROLLER:/home/heat-admin/
+
+for C in $CLUSTERS; do
+  ssh -q -o "StrictHostKeyChecking no" heat-admin@$CONTROLLER "bash test_ceph_client.sh $C"
+done
