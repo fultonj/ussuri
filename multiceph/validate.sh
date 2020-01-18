36c68a22c2fa976e16f05d0e6de4ff1ce8b3ab61
John Fulton <fulton@redhat.com>
2020-01-18 20:52:52 +0000

    update validate.sh to test ceph internal and ceph0,1 external

--- multiceph/validate.sh
+++ multiceph/validate.sh
@@ -3,7 +3,7 @@
 # Can $CONTROLLER use ceph cluster(s) named in $CLUSTERS?
 
 #CLUSTERS="ceph0"
-CLUSTERS="ceph0 ceph1"
+CLUSTERS="ceph ceph0 ceph1"
 FILE=test_ceph_client.sh
 source ~/stackrc
 CONTROLLER=$(openstack server list -c Networks -c Name -f value | grep controller-0 | awk {'print $2'} | sed s/ctlplane=//g)
