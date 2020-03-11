998eb88989954bbbcf7fa9f43ebcc4fb2d03e3ff
John Fulton <fulton@redhat.com>
2020-03-11 01:28:43 +0000

    glance POC working with manual intervention

--- glance/README.md
+++ glance/README.md
@@ -49,6 +49,12 @@ be deployed in the following stacks and roles.
 
 ## Verifications
 
+- Use [use-multistore-glance.sh](use-multistore-glance.sh) to import
+  an image into both `default_backend` and `dcn0`
+  with [import-multi-stores](https://review.opendev.org/#/c/667132)
+  and then copy that image to `dcn1`
+  with [copy-existing-image](https://review.opendev.org/#/c/696457).
+
 - Verify any DCN node at $IP can use the central ceph cluster
 ```
 scp ../multiceph/test_ceph_client.sh heat-admin@$IP:/home/heat-admin/
@@ -69,4 +75,52 @@ ssh $IP "sudo tail /var/lib/config-data/puppet-generated/glance_api/etc/glance/g
 
 ## Todo
 
-- Add script to upload image to multiple backends
+- Modify templates to configure all three glance services with
+  `enabled_import_methods = [glance-direct,web-download,copy-image]`
+- Modify [ceph_keys.sh](ceph_keys.sh) such that:
+  - `dcn0/dcn_ceph_keys.yaml` (from `ceph_keys.sh 2`) has:
+    ```
+      CephExternalMultiConfig:
+        - cluster: "central"
+      ...
+      ceph_conf_overrides:
+        client:
+          keyring: /etc/ceph/central.client.glance.keyring
+    ```
+  - `control-plane/ceph_keys_update.yaml` (from `ceph_keys.sh 3`) has:
+     ```
+      CephExternalMultiConfig:
+        - cluster: "dcn0"
+          fsid: "0c10d6b5-a455-4c4d-bd53-8f2b9357c3c7"
+          external_cluster_mon_ips: "172.16.11.45"
+          keys:
+            - name: "client.glance"
+              caps:
+                mgr: "allow *"
+                mon: "profile rbd"
+                osd: "profile rbd pool=images"
+              key: "AQDY215eAAAAABAAv+R9oML+7c8FFygX7eUWrQ=="
+              mode: "0600"
+          dashboard_enabled: false
+          ceph_conf_overrides:
+            client:
+              keyring: /etc/ceph/dcn0.client.glance.keyring
+        - cluster: "dcn1"
+          fsid: "8649d6c3-dcb3-4aae-8c19-8c2fe5a853ac"
+          external_cluster_mon_ips: "172.16.11.246"
+          keys:
+            - name: "client.glance"
+              caps:
+                mgr: "allow *"
+                mon: "profile rbd"
+                osd: "profile rbd pool=images"
+              key: "AQCSH19eAAAAABAAi1MVKfoJv5dcdWsQd4OG+Q=="
+              mode: "0600"
+          dashboard_enabled: false
+          ceph_conf_overrides:
+            client:
+              keyring: /etc/ceph/dcn1.client.glance.keyring
+      ```
+
+The current templates work but only after the above two configurations
+are added by hand.
---
+++ glance/use-multistore-glance.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+RC=control-plane/control-planerc
+if [[ -e $RC ]]; then
+    source $RC
+else
+    echo "$RC is missing. abort."
+    exit 1
+fi
+
+echo "Testing import-multi-stores and copy-existing-image"
+# -------------------------------------------------------
+echo "Check if glance is working"
+glance image-list
+if [[ $? -gt 0 ]]; then
+    echo "Aborting. Not even 'glance image-list' works."
+    exit 1
+fi
+# -------------------------------------------------------
+# Get image if missing
+NAME=cirros
+IMG=cirros-0.4.0-x86_64-disk.img
+RAW=$(echo $IMG | sed s/img/raw/g)
+URL=http://download.cirros-cloud.net/0.4.0/$IMG
+if [ ! -f $RAW ]; then
+    if [ ! -f $IMG ]; then
+        echo "Could not find qemu image $img; downloading a copy."
+        curl -L -# $URL > $IMG
+    fi
+    echo "Could not find raw image $raw; converting."
+    if [[ ! -e /bin/qemu-img ]]; then
+        sudo yum install qemu-img -y
+    fi
+    qemu-img convert -f qcow2 -O raw $IMG $RAW
+fi
+# -------------------------------------------------------
+OLD_ID=$(openstack image show $NAME -f value -c id)
+if [[ ! -z $OLD_ID ]]; then 
+    echo "- Clean out old image"
+    openstack image delete $OLD_ID
+fi
+# -------------------------------------------------------
+echo "- List available stores"
+glance stores-info
+
+echo "- Create image"
+glance --verbose image-create-via-import --disk-format raw --container-format bare --name $NAME
+ID=$(openstack image show $NAME -c id -f value)
+
+echo "- Upload image data to staging"
+glance --verbose image-stage $ID --file $RAW
+if [[ $? -gt 0 ]]; then
+    echo "Aborting. Unable to: glance image-stage $ID --file $RAW";
+    exit 1;
+fi
+
+echo "- Get token"
+TOKEN=$(openstack token issue -f value -c id)
+
+echo "- Get glance endpoint"
+ENDPOINT=$(openstack endpoint list -c "Service Name" -c "Interface" -c "URL" \
+                     -f value | grep glance | grep public | awk {'print $3'})
+
+# -------------------------------------------------------
+echo "Import image $NAME into both central and dcn0 (import-multi-stores) with curl"
+
+set -o xtrace
+curl -g -i -X POST $ENDPOINT/v2/images/$ID/import -H "User-Agent: python-glanceclient" -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -d '{"method": {"name": "glance-direct"}, "stores": ["default_backend", "dcn0"]}'
+set +o xtrace
+
+openstack image show $NAME
+
+echo "Looking at glance tasks"
+glance task-list | head -5
+
+echo "Show newest task"
+TASK_ID=$(glance task-list | head -4 | egrep "success|processing" | awk {'print $2'})
+glance task-show $TASK_ID
+echo "Confirm image ID in task"
+glance task-show $TASK_ID | grep $ID
+
+echo "Waiting for image to finish uploading or importing..."
+i=0
+while [ 1 ]; do
+    STATUS=$(openstack image show $NAME -f value -c status)
+    if [[ $STATUS == "importing" || $STATUS == "uploading" ]]; then
+        echo -n "."
+        sleep 5
+        i=$(($i+1))
+    else
+        break;
+    fi
+    if [[ $i -gt 20 ]]; then
+        echo "Giving up after $(($i * 5)) seconds"
+        exit 1
+    fi
+done
+
+if [[ -e ~/stackrc ]]; then
+    source ~/stackrc
+    CONTROLLER=$(openstack server list -f value -c Name -c Networks | grep controller | awk {'print $2'} | sed s/ctlplane=//g)
+fi
+if [[ ! -z CONTROLLER ]]; then
+    echo "- Use RBD to list images on central"
+    CMD_CENTRAL="sudo podman exec ceph-mon-\$(hostname) rbd --cluster central -p images ls -l"
+    ssh heat-admin@$CONTROLLER "$CMD_CENTRAL"
+    echo "- Use RBD to list images on dcn0"
+    CMD_DCN="sudo podman exec ceph-mon-\$(hostname) rbd --id glance --keyring /etc/ceph/dcn0.client.glance.keyring --conf /etc/ceph/dcn0.conf -p images ls -l"    
+    ssh heat-admin@$CONTROLLER "$CMD_DCN"
+fi
+
+source $RC
+echo ""
+echo "Copy image $NAME from central into dcn1 (copy-existing-image) with curl"
+echo ""
+
+set -o xtrace
+curl -g -i -X POST $ENDPOINT/v2/images/$ID/import -H "User-Agent: python-glanceclient" -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -d '{"method": {"name": "copy-image"}, "stores": ["dcn1"]}'
+set +o xtrace
+
+echo "Waiting 5 seconds for image to finish copying..."
+sleep 5
+
+if [[ ! -z CONTROLLER ]]; then
+    echo "- Use RBD to list images on dcn1"
+    CMD_DCN="sudo podman exec ceph-mon-\$(hostname) rbd --id glance --keyring /etc/ceph/dcn1.client.glance.keyring --conf /etc/ceph/dcn1.conf -p images ls -l"    
+    ssh heat-admin@$CONTROLLER "$CMD_DCN"
+fi
+
+echo "Show properties of $ID to see the stores"
+openstack image show $ID | grep properties
