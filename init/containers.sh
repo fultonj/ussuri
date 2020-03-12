2acc64bbe6faed2bac43dced16c7119a4ea51c64
John Fulton <fulton@redhat.com>
2020-03-12 20:55:13 +0000

    container.sh fail if file not created

--- init/containers.sh
+++ init/containers.sh
@@ -4,6 +4,9 @@ time sudo openstack tripleo container image prepare \
   -e containers.yaml \
   --output-env-file ~/containers-env-file.yaml
 
-#  -e ~/local_images.yaml \
+if [[ ! -e ~/containers-env-file.yaml ]]; then
+    echo "Failure: ~/containers-env-file.yaml was not created"
+    exit 1
+fi
 
 curl -s http://192.168.24.1:8787/v2/_catalog | jq "."
