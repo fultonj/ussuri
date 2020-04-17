6d6e3acf292c1ffc4c9c2426e11af26035ddee38
John Fulton <fulton@redhat.com>
2020-04-17 15:02:49 +0000

    add snapshots stub

---
+++ glance/validations/snapshots.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# -------------------------------------------------------
+# Test the following behaviors:
+# I.   DCN A/A Volume Snapshots?
+# II.  DCN Instance Snapshots to Images?
+# III. Push images created from instance snapshots back to central?
+# IV.  DCN Instance (booted from volume) Snapshots to Volumes?
+# -------------------------------------------------------
+
+AZ="dcn0"
+IMAGE=cirros
+
