f5f143069de2a2fbc1d00877f51503adaac73b64
John Fulton <fulton@redhat.com>
2020-04-21 00:47:59 +0000

    Testing SNAP_PET_TO_VOLUME
    This fails after I use dcn-pet.sh for both CINDER=1/NOVA=1 and
    CINDER=0/NOVA=1. Is this just not possible or am I doing it wrong?
    
    (control-plane) [CentOS-8.1 - stack@undercloud validations]$ ./snapshots.sh
    Testing SNAP_PET_TO_VOLUME
    Found server: 26a6822b-0301-41e8-94aa-667f3dfec085
    Found volume: 23e16765-82bc-4ed8-a313-fbf6c45cf3c1
    Stopping server (cannot shapshot while volume is in-use)
    Waiting for server to stop....
    Detaching volume (cannot snapshot a volume that is in-use)
    Cannot detach a root device volume (HTTP 400) (Request-ID: req-5a38280c-569f-4424-80d0-b03ea893001f)
    .....
    Unable to create snapshot: cinder volume is not available.
    Re-attaching volume and starting server
    Invalid volume: volume 23e16765-82bc-4ed8-a313-fbf6c45cf3c1 already attached (HTTP 400) (Request-ID: req-655a5e5e-e30c-401d-9d74-7cf3d3810186)
    +--------------------------------------+-----------------+--------+-----------------------------+-------+--------+
    | ID                                   | Name            | Status | Networks                    | Image | Flavor |
    +--------------------------------------+-----------------+--------+-----------------------------+-------+--------+
    | 26a6822b-0301-41e8-94aa-667f3dfec085 | pet-server-dcn0 | ACTIVE | private-dcn0=192.168.200.58 |       |        |
    +--------------------------------------+-----------------+--------+-----------------------------+-------+--------+
    +--------------------------------------+-----------------+--------+------+------------------------------------------+
    | ID                                   | Name            | Status | Size | Attached to                              |
    +--------------------------------------+-----------------+--------+------+------------------------------------------+
    | 23e16765-82bc-4ed8-a313-fbf6c45cf3c1 | pet-volume-dcn0 | in-use |    8 | Attached to pet-server-dcn0 on /dev/vda  |
    +--------------------------------------+-----------------+--------+------+------------------------------------------+
    
    (control-plane) [CentOS-8.1 - stack@undercloud validations]$

--- glance/validations/snapshots.sh
+++ glance/validations/snapshots.sh
@@ -2,12 +2,12 @@
 # -------------------------------------------------------
 # Use the following setting VARIABLE test each scenario
 # I.   DCN A/A Volume Snapshots?
-SNAP_TO_VOLUME=1
-# II.  DCN Instance Snapshots to Images?
+SNAP_TO_VOLUME=0
+# II.  DCN Instance (booted from volume) Snapshots to Volumes?
+SNAP_PET_TO_VOLUME=1
+# III.  DCN Instance Snapshots to Images?
 SNAP_TO_IMAGE=0
-# III.  DCN Instance (booted from volume) Snapshots to Volumes?
-SNAP_PET_TO_VOLUME=0
-# IV. Push image created from instance snapshot (II) back to central?
+# IV. Push image created from instance snapshot (III) back to central?
 PUSH=0
 # -------------------------------------------------------
 AZ="dcn0"
@@ -52,12 +52,63 @@ if [[ $SNAP_TO_VOLUME -eq 1 ]]; then
     openstack volume snapshot list
 fi
 # -------------------------------------------------------
-if [[ $SNAP_TO_IMAGE -eq 1 ]]; then
-    echo "Testing SNAP_TO_IMAGE"
-fi
-# -------------------------------------------------------
 if [[ $SNAP_PET_TO_VOLUME -eq 1 ]]; then
     echo "Testing SNAP_PET_TO_VOLUME"
+    BASE=pet-server-$AZ
+    SNAP=pet-server-$AZ-snapshot
+    NOVA_ID=$(openstack server show $BASE -f value -c id)
+    if [[ ! $(echo $NOVA_ID | grep -E "[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}" | wc -l) -eq 1 ]]; then
+        echo "Unable to find $BASE. Please run dcn-pet.sh"
+        exit 1
+    fi
+    CINDER_ID=$(openstack volume list -f value -c ID -c "Attached to" | grep $NOVA_ID | awk {'print $1'} | head -1)
+    echo "Found server: $NOVA_ID"
+    echo "Found volume: $CINDER_ID"
+    echo "Stopping server (cannot shapshot while volume is in-use)"
+    openstack server stop $NOVA_ID
+    i=0
+    STATUS=$(openstack server show $NOVA_ID  -f value -c status)
+    echo -n "Waiting for server to stop"
+    while [[ $STATUS == "ACTIVE" ]]; do
+        echo -n "."
+        sleep 1
+        i=$(($i+1))
+        if [[ $i -gt 30 ]]; then break; fi
+        STATUS=$(openstack server show $NOVA_ID  -f value -c status)
+    done
+    echo "."
+    if [[ $STATUS != "SHUTOFF" ]]; then
+        echo "Server is not cleanly SHUTOFF. Exiting."
+        exit 1
+    fi
+    i=0
+    echo "Detaching volume (cannot snapshot a volume that is in-use)"
+    openstack server remove volume $NOVA_ID $CINDER_ID
+    STATUS=$(openstack volume show $CINDER_ID  -f value -c status)
+    while [[ $STATUS == "in-use" ]]; do
+        echo -n "."
+        sleep 1
+        i=$(($i+1))
+        if [[ $i -gt 3 ]]; then break; fi
+        STATUS=$(openstack volume show $CINDER_ID  -f value -c status)
+    done
+    echo "."
+    if [[ $STATUS != "available" ]]; then
+        echo -e "Unable to create snapshot: cinder volume is not available."
+    else
+        echo "Creating snapshot $SNAP"
+        openstack volume snapshot create $SNAP --volume $CINDER_ID
+    fi
+    echo "Re-attaching volume and starting server"
+    openstack server add volume $NOVA_ID $CINDER_ID
+    openstack server start $NOVA_ID
+    openstack server list
+    openstack volume list
+    openstack volume snapshot list
+fi
+# -------------------------------------------------------
+if [[ $SNAP_TO_IMAGE -eq 1 ]]; then
+    echo "Testing SNAP_TO_IMAGE"
 fi
 # -------------------------------------------------------
 if [[ $PUSH -eq 1 ]]; then
